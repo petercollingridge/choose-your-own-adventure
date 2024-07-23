d2de8c0d2ac5bc32799a4ef4e168be55804d7587
Peter Collingridge <peter.collingridge@nanoporetech.com>
2024-07-23 10:14:31 +0100

    Add player state and dynamic text

--- main.js
+++ main.js
@@ -1,30 +1,96 @@
+const player = {
+  health: 100,
+  gold: 10,
+};
+
 const pages = {
-  1: {
+  start: {
     text: 'This is the start of the adventure',
-    next: {
-      2: 'Go left',
-      3: 'Go right',
-    },
+    next: [
+      { text: 'Go to the shop', goto: 'shop' },
+      { text: 'Go to the cave', goto: 'cave' },
+      { text: 'Go to the wood', goto: 'wood' },
+    ],
   },
-  2: {
-    text: 'You went to left',
-    next: {
-      1: 'Go back',
-      4: 'Go to the end',
-    },
+  cave: {
+    text: 'The cave is empty.',
+    next: [
+      { text: 'Go back', goto: 'start' },
+    ],
   },
-  3: {
-    text: 'You went to right',
-    next: {
-      1: 'Go back',
-      4: 'Go to the end',
-    },
+  shop: {
+    text: () => `The shop sells weapons. You have ${player.gold} gold.`,
+    next: [
+      {
+        text: 'Buy an axe for 8 gold',
+        goto: 'axe',
+        if: () => player.gold >= 8,
+      },
+      {
+        text: 'Buy a dagger for 4 gold',
+        goto: 'dagger',
+        if: () => player.gold >= 4,
+      },
+      {
+        text: 'Buy a vorpal sword for 12 gold',
+        goto: 'sword',
+        if: () => player.gold >= 12
+      },
+      { text: 'Go back', goto: 'start' },
+    ],
   },
-  4: {
-    text: 'The end',
-    next: {
-      1: 'Start again',
+  axe: {
+    onload: () => {
+      player.gold -= 8;
+      player.axe = true;
     },
+    text: 'You now have an axe',
+    next: [
+      { text: 'Continue', goto: 'shop' },
+    ],
+  },
+  dagger: {
+    onload: () => {
+      player.gold -= 4;
+      player.dagger = true;
+    },
+    text: 'You now have a dagger',
+    next: [
+      { text: 'Continue', goto: 'shop' },
+    ],
+  },
+  sword: {
+    onload: () => {
+      player.gold -= 12;
+      player.sword = true;
+    },
+    text: 'You now have a sword',
+    next: [
+      { text: 'Continue', goto: 'shop' },
+    ],
+  },
+  wood: {
+    text: 'You find a bear',
+    next: [
+      { text: 'Run away', goto: 'dead' },
+      {
+        text: 'Fight it',
+        goto: 'win', if:
+        () => player.axe || player.dagger || player.sword,
+      },
+    ],
+  },
+  dead: {
+    text: 'The chases you, catches you, and eats you.',
+    next: [
+      { text: 'Start again', goto: 'start' },
+    ],
+  },
+  win: {
+    text: 'You defeat the bear and win the game.',
+    next: [
+      { text: 'Start again', goto: 'start' },
+    ],
   },
 };
 
@@ -34,19 +100,28 @@ const optionsDiv = document.getElementById('options');
 function goToPage(page) {
   const pageData = pages[page];
 
+  // Call onLoad function if there is one
+  if (pageData.onload) {
+    pageData.onload();
+  }
+
   // Set text
-  mainText.textContent = pageData.text;
+  const text = typeof pageData.text === 'function' ? pageData.text() : pageData.text;
+  mainText.textContent = text;
 
   // Clear any existing options
   optionsDiv.innerHTML = '';
 
   // Create new options
-  Object.entries(pageData.next || {}).forEach(([key, value]) => {
-    const div = document.createElement('div');
-    div.textContent = value;
-    div.onclick = () => goToPage(key);
-    optionsDiv.appendChild(div);
+  (pageData.next || []).forEach((next) => {
+    // Check for conditional options
+    if (!next.if || next.if()) {
+      const div = document.createElement('div');
+      div.textContent = next.text;
+      div.onclick = () => goToPage(next.goto);
+      optionsDiv.appendChild(div);
+    }
   });
 }
 
-goToPage(1);
+goToPage('start');
